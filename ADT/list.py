57211809431c1b38ece94cac7fca2a78f85d9a59
juan123E <j.quirogar@uniandes.edu.co>
2020-08-23 05:56:48 -0500

    hola

--- ADT/list.py
+++ ADT/list.py
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  """
-
+"hola"
 import config
 from Utils import error as error
 from DataStructures import liststructure as lt
